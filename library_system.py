e81b7e0365579730219e9339dbc15ad52467fbb7
jlake70 <jlake7021@gmail.com>
2024-08-14 10:17:24 -0400

    Completed library system file

---
+++ library_system.py
@@ -0,0 +1,46 @@
+class BookOperations:
+    def add_book(self):
+        print("Adding a new book...")
+        
+
+    def borrow_book(self):
+        print("Borrowing a book...")
+        
+
+    def return_book(self):
+        print("Returning a book...")
+        
+
+    def search_book(self):
+        print("Searching for a book...")
+        
+
+    def display_books(self):
+        print("Displaying all books...")
+        
+
+class UserOperations:
+    def add_user(self):
+        print("Adding a new user...")
+        
+
+    def view_user(self):
+        print("Viewing user details...")
+        
+
+    def display_users(self):
+        print("Displaying all users...")
+        
+
+class AuthorOperations:
+    def add_author(self):
+        print("Adding a new author...")
+        
+
+    def view_author(self):
+        print("Viewing author details...")
+       
+
+    def display_authors(self):
+        print("Displaying all authors...")
+      
---
+++ main.py
@@ -0,0 +1,1 @@
+from library_system import BookOperations, UserOperations, AuthorOperations
